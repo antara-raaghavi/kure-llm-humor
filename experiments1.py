3df8f8b7fbffe98640e236cd1b3b29f3da24cec8
Antara <antara.raaghavi@gmail.com>
2024-04-23 12:56:30 -0400

    testing batch files

---
+++ experiments1.py
@@ -0,0 +1,27 @@
+import gpt_2_simple as gpt2
+import os
+import sys
+
+model_name = "124M"
+
+if not os.path.isdir(os.path.join("models", model_name)):
+	print(f"Downloading {model_name} model...")
+	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
+
+file_name = sys.argv[1]
+
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess,
+              file_name,
+              model_name=model_name,
+              steps=40,
+              restore_from='fresh',
+              run_name=f'run {file_name}',
+              print_every=5,
+              sample_every=10,
+              save_every=20
+              )
+
+gpt2.generate(sess)
+
+
