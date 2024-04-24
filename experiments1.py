7f307cf81374a9c7060743834ca01d0a92fb955b
Antara <antara.raaghavi@gmail.com>
2024-04-24 17:35:05 -0400

    slurm worksgit add .!

--- experiments1.py
+++ experiments1.py
@@ -3,12 +3,14 @@ import os
 import sys
 
 model_name = "124M"
+file_name = sys.argv[1]
+print(file_name)
 
 if not os.path.isdir(os.path.join("models", model_name)):
 	print(f"Downloading {model_name} model...")
 	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
-file_name = sys.argv[1]
+
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
