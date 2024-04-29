365be192be9f7427a7b84051cef2e69873ff783d
Antara <antara.raaghavi@gmail.com>
2024-04-29 15:30:40 -0400

    updated w/ processing etc!

--- experiments2.py
+++ experiments2.py
@@ -1,44 +1,12 @@
-import gpt_2_simple as gpt2
-import os
 import sys
-from datetime import datetime
-
-model_name = "124M"
+n_steps = 30
 file_name = sys.argv[1]
-print(file_name)
 
-now = datetime.now()
-
-if not os.path.isdir(os.path.join("models", model_name)):
-	print(f"Downloading {model_name} model...")
-	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
-
-run = f'run {file_name} NEW'
-
-n_steps = 12
-
-sess = gpt2.start_tf_sess()
-gpt2.finetune(sess,
-              file_name,
-              model_name=model_name,
-              steps=n_steps,
-              restore_from='fresh',
-              run_name=run,
-              print_every=5,
-              sample_every=1,
-              save_every=20
-              )
-
-# gpt2.generate(sess)
-
-
-#postprocessing! 
-
-for i in range(2, n_steps):
-    output_path = f'~/samples/run {file_name} NEW/samples-{i}'  
-    clean_output_path =  f'~/samples/run {file_name} NEW/CLEAN-samples-{i}'  
+for i in range(3, n_steps, 2):
+    output_path = f'samples/run {file_name} 26Apr/samples-{i}'  
+    clean_output_path =  f'samples/run {file_name} 26Apr/CLEAN-samples-{i}'  
     input_path = file_name
-    final_output_path = f'~/samples/run {file_name} NEW/PROCESSED-samples-{i}'  
+    final_output_path = f'samples/run {file_name} 26Apr/PROCESSED-samples-{i}'  
     seen = set()
     inputs = set()
     cleaned = []
@@ -70,5 +38,3 @@ for i in range(2, n_steps):
             else:
                 f4.write(line + '\n')
         f4.write("Percentage present in input file: " + str(ctr/total))
-
-
